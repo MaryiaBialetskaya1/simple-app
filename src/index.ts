9c66106b78d2048c8c37d97e8280d03deec994a4
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-10-15 20:19:42 -0400

    edit text

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
 
-    let helloMessage = "Hello my dear Incubator!!!"
+    let helloMessage = "Hello my dear Incubator!!! Missed you!"
     res.send(helloMessage)
 })
 
